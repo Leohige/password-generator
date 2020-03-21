3bb2f9477c994463d66e016347361745104e1efc
Leandro Matheus <leohige@outlook.com>
2020-03-21 19:17:38 -0300

    Try to fix absolute path at build deploy again

--- vue.config.js
+++ vue.config.js
@@ -22,5 +22,8 @@ module.exports = {
   },
   publicPath: process.env.NODE_ENV === 'production'
   ? '/password-generator/'
-  : '/'
+  : '/',
+  assetsDir: process.env.NODE_ENV === 'production'
+  ? 'password-generator'
+  : ''
 };
